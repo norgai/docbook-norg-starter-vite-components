8ccaa0091f64febcb258abc976989795c1447934
TomTyack <thomas.tyack@conversiondigital.com.au>
2025-06-02 22:04:25 +1000

    component_files

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+        'display': ['Inter', 'sans-serif'],
+        'inter': ['"Inter"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
