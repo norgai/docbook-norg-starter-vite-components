a83e509d722816ae711109072935658ddede7d02
TomTyack <thomas.tyack@conversiondigital.com.au>
2025-06-13 09:39:40 +1000

    remove unused

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { useState } from 'react';
+// import { Link } from 'react-router-dom';
 
 /**
  * Home page component
@@ -10,7 +10,7 @@ export default function Home() {
   
   return (
     <div>
-      
+      Homepage
     </div>
   );
 }
