997df9a4880e47cb7cb2814e1eadc42a24ff0eac
TomTyack <thomas.tyack@conversiondigital.com.au>
2025-06-18 09:51:43 +1000

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,10 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 0,            // 👈 let Vite auto-select
+    strictPort: false,  // default; keeps trying 5174, 5175…
+    allowedHosts: true  // accept requests for any hostname
+  }
 })
