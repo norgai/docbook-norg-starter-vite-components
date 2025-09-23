e903bd84d91daa89379c78af16b0766ebf02adbe
Thomas Tyack <thomas@norg.ai>
2025-09-23 05:21:17 +0000

    chore(components): update AssuranceStrip [automated]

---
+++ src/pages/theme/conversion/components/AssuranceStrip/mottocomponent/page.tsx
@@ -0,0 +1,11 @@
+
+import MottoComponent from '../../../../../components/theme/conversion/components/mottocomponent/figmaToCode';
+
+/**
+ * MottoComponent Page - Displays the MottoComponent in isolation
+ */
+export default function MottoComponentPage() {
+  return (
+    <MottoComponent />
+  );
+}
